0c3a1fd04353263c4a050a0a25aab5bfac0c98ef
fjb <developer@QuestorSystems.com>
2012-01-23 13:34:26 +0100

    First commint from new LQSAAA06 workstation

--- mount_debian.pl
+++ mount_debian.pl
@@ -3,22 +3,22 @@
 use warnings;
 use strict;
 
-my $base = '/mnt';
+my $base  = '/mnt';
 my $count = 5;
+my $ip    = '192.168.219.25';
 
-foreach my $cd (1 .. 5){
-	my $path = $base . "/debian$cd";
-	if (! -d $path){
-		print "creating dir: $path\n";
-	
-		# create the mount directories
-		`mkdir $path`;
+foreach my $cd ( 1 .. 5 ) {
+    my $path = $base . "/debian$cd";
+    if ( !-d $path ) {
+        print "creating dir: $path\n";
 
-		}
+        # create the mount directories
+        `mkdir $path`;
 
-	# mount the ISO image
-	print "mounting ISO $cd\n";
-	`mount -t nfs 192.168.0.5:debian$cd $path`;
+    }
 
-	}
+    # mount the ISO image
+    print "mounting ISO $cd\n";
+    `mount -t nfs $ip:debian$cd $path`;
 
+}
