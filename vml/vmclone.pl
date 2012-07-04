9323a419abe568e2bba970f2a3c921164f9b77d9
fjb <developer@QuestorSystems.com>
2012-07-04 18:02:45 +0200

    working stable version of vmclone.pl - excellent deployment too for VML

--- vml/vmclone.pl
+++ vml/vmclone.pl
@@ -1,30 +1,39 @@
-#!/usr/bin/perl -w 
-# #!/usr/bin/env perl -w
+#!/usr/bin/perl 
 
 use strict;
 use warnings;
 
-my $basedir = '/share/SHRDATA/PROD/datastores/ds01n-nqsaaa01';
-my $template = shift; 
-my $hostname = shift; 
+# define vaiables
+my $basedir  = '/share/SHRDATA/PROD/datastores/ds01n-nqsaaa01';
+my $template = shift;
+my $hostname = shift;
 
+if ( !defined($template) ) { usage() }
+if ( !defined($hostname) ) { usage() }
 
 # rename the template files
-while (my $file = <$template*>){
+while ( my $file = <$template*> ) {    # foreach file that matches template
 
-	
-	my $newfile = $file;
-	$newfile =~ s/$template/$hostname/;
+    my $newfile = $file;
+    $newfile =~ s/$template/$hostname/;
 
-	print "renaming: $file -> $newfile\n";
-	system ("mv $file $newfile");
+    print "renaming: $file -> $newfile\n";
+    system("mv $file $newfile");
 }
-
 
 # VMX configuration file - modify
 # change all occurances of $template -> $hostname in files *.vmdk, *.vmxf, *.vmx
 
-  print "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmdk\n";
-  print "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmxf\n";
-  print "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmx\n";
+print
+    "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmdk\n";
+print
+    "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmxf\n";
+print
+    "execute:  perl -wpl -i.bak -e 's/$template/$hostname/g;' $hostname.vmx\n";
 
+sub usage {
+
+    print "usage: $0 <template_name> <target_machine_name>\n";
+    exit 10;
+
+}
